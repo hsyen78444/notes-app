947567b959208585dc1dcb7d4091234afc981878
Your Name <you@example.com>
2025-12-01 22:41:21 -0500

    added dockerfile

---
+++ backend/src/main/java/com/notesapp/backend/WebConfig.java
@@ -0,0 +1,17 @@
+package com.notesapp.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins("https://notes-app-ivory-mu.vercel.app/", "http://localhost:8080", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
